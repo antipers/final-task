3f71479d7b7b93e50d0343122f5534ba9f832ad9
antipers <antipers@gmail.com>
2022-08-09 20:14:49 +0300

    solved

--- Program.cs
+++ Program.cs
@@ -6,5 +6,13 @@ for (int i = 0; i < array.Length; i++)
 }
 
 System.Console.WriteLine("Представлен массив состоящий из следующих элементов");
-    System.Console.WriteLine("[" + string.Join(" , ", array) + "]");
+System.Console.WriteLine("[" + string.Join(" , ", array) + "]");
 
+System.Console.WriteLine("Элементы с количеством символов меньше 3-");
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        System.Console.Write($"[ {array[i]} ]");
+    }
+}
